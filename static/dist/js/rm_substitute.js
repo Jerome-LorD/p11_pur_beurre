9fb6140f4cdd1128085b6c654e06c7d690d3b06c
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-11 11:53:42 +0200

    add new rm_substitute.js

---
+++ static/dist/js/rm_substitute.js
@@ -0,0 +1,46 @@
+/*
+Remove substitute from favorite page.
+*/
+
+async function postJsonData(url, data, headers) {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: headers
+      });
+      return await response.json();
+  
+    } catch (err) {
+      return console.warn(err);
+    }
+  }
+  
+  buttons = document.querySelectorAll(".btn-outline-warning")
+    .forEach((button) => {
+      button.addEventListener("click", (event) => {
+        let prodbox = document.getElementById("fav-" + event.target.value)
+        prodbox.parentNode.removeChild(prodbox);
+  
+        postJsonData(ajax_url, {
+            "products": event.target.value,
+            "ref_product_id": "",
+            "status": "unsave"
+          }, {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          })
+          .then(jsonResponse => {
+            let reference_id = jsonResponse.reference_id;
+            postJsonData(ajax_url, {
+              "products": event.target.value,
+              "ref_product_id": reference_id,
+              "status": "unsave"
+            }, {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+            })
+          })
+      });
+  
+    });
