3f5aa91d7b89a3a61558a69f8b61229f1a999d4f
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-13 08:47:24 +0200

    refactoring

--- static/dist/js/get_substitutes.js
+++ static/dist/js/get_substitutes.js
@@ -25,16 +25,44 @@ async function postJsonData(url, data, headers) {
         postJsonData(ajax_url, {
           "products": checkbox.value,
           "ref_product_id": ref_product_id,
-          "status": "save"
+          "status": true
         }, {
           "Accept": "application/json",
           "Content-Type": "application/json"
+        })
+        .then(jsonResponse => {
+            let max_in_db_reached = jsonResponse.max_in_db_reached;
+            console.log("MAX", max_in_db_reached);
+            if (max_in_db_reached == true) {
+                let overlay = document.getElementById('warn-popup');
+                // document.querySelector("input[type=checkbox][value=checkbox.value]").click();
+                // ckbx_reverse.click();
+
+                // let warn = "Vous avez atteint le nombre maximum de substituts en favoris.";
+                // let p_warn = document.createElement("p");
+                // p_warn.className = "warn-reached";
+                // let p_warnContent = document.createTextNode(warn);
+                // p_warn.appendChild(p_warnContent);
+                // document.querySelector("#reached").appendChild(p_warn);
+                overlay.style.visibility='visible';
+                overlay.style.opacity=1;
+                for(ckbx_reverse in document.querySelector("input[type=checkbox][value="+checkbox.value+"]")) {
+                    console.log("base", ckbx_reverse.value);
+                    if(ckbx_reverse.value == checkbox.value) {
+                        // document.getElementsByClassName("product").click();
+                        console.log(ckbx_reverse.value);
+                    }
+                 }
+                
+                // document.querySelector("input[type=checkbox][value="+checkbox.value+"]").click();
+                // console.log(document.getElementsByClassName("product"));
+            }
         })
       } else {
         postJsonData(ajax_url, {
           "products": checkbox.value,
           "ref_product_id": ref_product_id,
-          "status": ""
+          "status": false
         }, {
           "Accept": "application/json",
           "Content-Type": "application/json"
@@ -42,5 +70,13 @@ async function postJsonData(url, data, headers) {
       }
     })
   });
+
+
+let exit = document.querySelector(".exit")
+exit.addEventListener("click", function(e) {
+    let overlay = document.getElementById('warn-popup');
+    overlay.style.visibility='hidden';
+    overlay.style.opacity=0;
+})
 
 
