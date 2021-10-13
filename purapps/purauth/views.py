6b61f72f7bb345181d34a1457e10440f51f90116
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-13 08:37:15 +0200

    adding premium view

--- purapps/purauth/views.py
+++ purapps/purauth/views.py
@@ -1,10 +1,13 @@
 """Purauth module."""
+import json
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import InscriptForm, NewLoginForm
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
 
 
 def inscript(request):
@@ -51,3 +54,18 @@ def user_logout(request):
     """Log out."""
     logout(request)
     return redirect("index")
+
+
+@csrf_exempt
+def premium(request):
+    """Modify user status."""
+    data = json.loads(request.body)
+    is_premium = data["is_premium"]
+
+    if is_premium:
+        user = User.objects.get(id=request.user.id)
+        if user.is_premium is False:
+            user.is_premium = True
+            user.save()
+
+    return render(request, "registration/profile.html")
