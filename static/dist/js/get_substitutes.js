20c366b459f9022324f71de14aa8d1263bf8df7b
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-14 18:39:32 +0200

    refactoring

--- static/dist/js/get_substitutes.js
+++ static/dist/js/get_substitutes.js
@@ -22,7 +22,7 @@ async function postJsonData(url, data, headers) {
     checkbox.addEventListener("change", function(e) {
       e.preventDefault();
       if (checkbox.checked == true) {
-        postJsonData(ajax_url, {
+        postJsonData(ajax_url_save_substitutes, {
           "products": checkbox.value,
           "ref_product_id": ref_product_id,
           "status": true
@@ -32,34 +32,15 @@ async function postJsonData(url, data, headers) {
         })
         .then(jsonResponse => {
             let max_in_db_reached = jsonResponse.max_in_db_reached;
-            console.log("MAX", max_in_db_reached);
             if (max_in_db_reached == true) {
                 let overlay = document.getElementById('warn-popup');
-                // document.querySelector("input[type=checkbox][value=checkbox.value]").click();
-                // ckbx_reverse.click();
-
-                // let warn = "Vous avez atteint le nombre maximum de substituts en favoris.";
-                // let p_warn = document.createElement("p");
-                // p_warn.className = "warn-reached";
-                // let p_warnContent = document.createTextNode(warn);
-                // p_warn.appendChild(p_warnContent);
-                // document.querySelector("#reached").appendChild(p_warn);
+                checkbox.checked = false;
                 overlay.style.visibility='visible';
                 overlay.style.opacity=1;
-                for(ckbx_reverse in document.querySelector("input[type=checkbox][value="+checkbox.value+"]")) {
-                    console.log("base", ckbx_reverse.value);
-                    if(ckbx_reverse.value == checkbox.value) {
-                        // document.getElementsByClassName("product").click();
-                        console.log(ckbx_reverse.value);
-                    }
-                 }
-                
-                // document.querySelector("input[type=checkbox][value="+checkbox.value+"]").click();
-                // console.log(document.getElementsByClassName("product"));
             }
         })
       } else {
-        postJsonData(ajax_url, {
+        postJsonData(ajax_url_delete_substitutes, {
           "products": checkbox.value,
           "ref_product_id": ref_product_id,
           "status": false
