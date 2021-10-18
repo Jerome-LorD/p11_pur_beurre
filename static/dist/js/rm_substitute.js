e260066e84e0f5ec69af566dec37af46edf027b4
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-19 00:01:43 +0200

    refactoring

--- static/dist/js/rm_substitute.js
+++ static/dist/js/rm_substitute.js
@@ -1,41 +1,41 @@
 async function postJsonData(url, data, headers) {
-    try {
+  try {
       const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: headers
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: headers
       });
       return await response.json();
-  
-    } catch (err) {
+
+  } catch (err) {
       return console.warn(err);
-    }
   }
-  
-  buttons = document.querySelectorAll(".btn-outline-warning")
-    .forEach((button) => {
+}
+
+buttons = document.querySelectorAll(".btn-outline-warning")
+  .forEach((button) => {
       button.addEventListener("click", (event) => {
-        let prodbox = document.getElementById("fav-" + event.target.value);
-        prodbox.parentNode.removeChild(prodbox);
-  
-        postJsonData(ajax_url_get_reference, {
-            "product_id": event.target.value,
-            "reference_id": ""
-          }, {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken
-          })
-          .then(jsonResponse => {
-            let reference_id = jsonResponse.reference_id;
-            postJsonData(ajax_url_delete_substitutes, {
-              "product_id": event.target.value,
-              "reference_id": reference_id
-            }, {
-              "Accept": "application/json",
-              "Content-Type": "application/json",
-              "X-CSRFToken": csrftoken
-            })
-          })
+          let prodbox = document.getElementById("fav-" + event.target.value);
+          prodbox.parentNode.removeChild(prodbox);
+
+          postJsonData(ajax_url_get_reference, {
+                  "product_id": event.target.value,
+                  "reference_id": ""
+              }, {
+                  "Accept": "application/json",
+                  "Content-Type": "application/json",
+                  "X-CSRFToken": csrftoken
+              })
+              .then(jsonResponse => {
+                  let reference_id = jsonResponse.reference_id;
+                  postJsonData(ajax_url_delete_substitutes, {
+                      "product_id": event.target.value,
+                      "reference_id": reference_id
+                  }, {
+                      "Accept": "application/json",
+                      "Content-Type": "application/json",
+                      "X-CSRFToken": csrftoken
+                  })
+              })
       });
-    });
+  });
