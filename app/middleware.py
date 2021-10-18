8811d2758471a1e729e5b55828fbcf741d36e894
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-10-18 23:41:50 +0200

    refactoring & add PremiumMiddleware

--- app/middleware.py
+++ app/middleware.py
@@ -1,5 +1,6 @@
 """Middleware module."""
 from purapps.purbeurre.forms import SearchProduct
+from purapps.purauth.forms import PremiumForm
 from django.shortcuts import redirect
 
 
@@ -19,4 +20,28 @@ class SearchMiddleware:
             return redirect("results", product_name=product_name)
 
         request.search_form = search_form
+        return self.get_response(request)
+
+
+class PremiumMiddleware:
+    """PremiumMiddleware class."""
+
+    def __init__(self, get_response):
+        """Init."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Call."""
+        premium_form = PremiumForm()
+        if request.method == "POST":
+            premium_form = PremiumForm(request.POST)
+
+            if premium_form.is_valid():
+                premium_form.clean_premium()
+
+                request.user.is_premium = True
+                request.user.save()
+
+        request.premium_form = premium_form
+
         return self.get_response(request)
